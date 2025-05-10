ec811ca3a23951ab69dafc6a1db5f4bba8bf9134
pratibha <kp257206@gmail.com>
2025-05-10 07:28:33 +0530

    1st push

---
+++ app/src/main/java/com/example/pb/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.pb;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // You'll need to create this layout too
+    }
+}
